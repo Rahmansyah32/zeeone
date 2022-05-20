603b1e84fe4e4ad5e8d92a58d839a5f0770564da
FajarXN <xenongege@gmail.com>
2022-05-20 20:27:47 +0700

    Alphabot-Md

---
+++ settings.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	alfa: 'https://api-alphabot.herokuapp.com',
+}
+
+// Free apikey
+global.APIKeys = {
+	'https://api-alphabot.herokuapp.com': 'Alphabot',
+}
+
+// setting 
+global.autoread = true // auto read pesan / message
+global.autorecording = true //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+
+// Other
+global.botname = "Quinx-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ FajarGanzོ ×፝֟͜×" //nama kalian
+global.myweb ="https://instagram.com/fajar.nrd" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UC-_PgO4W2W01BRJo47bly1g" //bebas asal jan hapus
+global.github = "https://github.com/FajarXN/Fajar" //bebas
+global.email = "fajaren3@gmail.com" //bebas
+global.region = "Indonesia" //bebas
+global.ownernomer = "6285223272352" // nomor wa kalian
+global.ownernomerr = "+6285223272352" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.owner = ["6285223272352","6285223272352","62852232723523"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© QUINX-BOTོ' //sticker wm ubah
+global.author = 'By FajarGanteng ig : @fajar.nrd' //sticker wm ganti nama kalian
+global.sessionName = 'session'
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    sukses: '🤗Done, Onichan~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
